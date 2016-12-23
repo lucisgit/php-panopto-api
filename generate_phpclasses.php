9dbf0e9ee963346f96745c39b9f098ccfd101f4e
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2016-12-23 15:11:01 +0000

    Add optional parameters to generate_phpclasses.php
    It is possible to specify hostname and API version in CLI.

--- generate_phpclasses.php
+++ generate_phpclasses.php
@@ -2,8 +2,8 @@
 
 /*
  * Panopto PHP classes generator.
- *
- * This tool is using WSDL to PHP classes converter tool
+ * 
+ * This script is using WSDL to PHP classes converter tool
  * (https://github.com/wsdl2phpgenerator/wsdl2phpgenerator) to
  * generate PHP classess for Panopto API web services use.
  */
@@ -21,8 +21,52 @@ if (file_exists(__DIR__.'/vendor/autoload.php')) {
     die;
 }
 
+// Set defaults.
 $server = 'demo.hosted.panopto.com';
 $version = '4.6';
+
+// CLI options.
+$options = getopt("h", array('host:', 'apiversion:', 'help'));
+
+// Checking util.php CLI script usage.
+$help = <<<HELP
+
+Panopto PHP classes generator.
+
+This script is generating PHP classess for Panopto API web services using WSDL interface.
+
+Usage:
+  php generate_phpclasses.php [-h|--help] [--host=value] [--apiversion=value]
+
+Options:
+--host       Hostname to use for WSDL endpoints (default: $server).
+--apiversion API version to use (default: $version).
+
+-h, --help       Print out this help
+
+Example usage:
+\$ php generate_phpclasses.php --host=panopto.host.org --apiversion=4.6
+
+The above command will generate PHP classes in ./lib/Panopto/PublicAPI/4.6/ directory.
+
+HELP;
+
+// Read CLI options.
+if (isset($options['help']) || isset($options['h'])) {
+    echo $help;
+    exit(0);
+}
+
+if (!empty($options['host'])) {
+    $server = $options['host'];
+}
+
+if (!empty($options['apiversion'])) {
+    $version = $options['apiversion'];
+}
+
+// Generate classes.
+$destination = __DIR__.'/lib/Panopto/PublicAPI/' . $version;
 
 $webservices = array(
     'AccessManagement',
@@ -33,17 +77,19 @@ $webservices = array(
     'UserManagement',
 );
 
-echo 'Using https://' . $server . '/Panopto/PublicAPI/' . $version . "/ for WSDL endpoints.\n";
+echo 'Using https://' . $server . '/Panopto/PublicAPI/' . $version . "/ for webservices interface.\n";
+
 foreach ($webservices as $webservice) {
     echo "Generating \Panopto\\" . $webservice . " classes...\n";
     $generatorconfig = array(
         'inputFile' => 'https://' . $server . '/Panopto/PublicAPI/' . $version . '/' . $webservice . '.svc?wsdl',
-        'outputDir' => __DIR__.'/lib/Panopto/PublicAPI/' . $version . '/' . $webservice,
+        'outputDir' => $destination . '/' . $webservice,
         'namespaceName' => 'Panopto\\' . $webservice,
     );
 
     $generator->generate(new \Wsdl2PhpGenerator\Config($generatorconfig));
 }
 
-echo "\nPHP classess have been generated from Panopto API. Use lib/Panopto/PublicAPI/" . $version . "/<webservice>/autoload.php in your project.\n";
+echo "\nPHP classess have been generated from Panopto API WSDL. Use " . $destination . "/<webservice>/autoload.php in your project.\n";
 exit(0);
+
